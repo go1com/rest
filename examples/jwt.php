7ddadf58c1237a07f8c67e51ae820faac37f559f
Andy Truong <thehongtt@gmail.com>
2019-03-01 10:27:40 +1000

    Fix test case - continue: missing file.

--- examples/jwt.php
+++ examples/jwt.php
@@ -25,30 +25,22 @@ use go1\rest\RestService;
  * JWT=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c
  * curl localhost:8989/hello/there?jwt=$JWT
  */
-if (!function_exists('__main__')) {
-    function __main__()
-    {
+call_user_func(
+    function () {
         $app = new RestService();
 
-        $app->get('/hello/{name}', function (Request $request, Response $res, string $name) {
+        $app->get('/hello/{name}', function (Request $request, Response $res) {
             return $res->withJson([
-                'name'       => [
-                    'fromArgument' => $name,
-                    'fromRoute'    => $request->getAttribute('routeInfo')[2]['name'],
-                ],
-                'jwt'        => [
+                'jwt' => [
                     'contextUser'            => $request->contextUser(),
                     'isSystemUser'           => $request->isSystemUser(),
                     'isPortalAdmin'          => $request->isPortalAdmin('qa.mygo1.com'),
                     'isPortalManager'        => $request->isPortalManager('qa.mygo1.com'),
                     'isPortalContentManager' => $request->isPortalContentAdministrator('qa.mygo1.com'),
                 ],
-                'attributes' => $request->getAttributes(),
             ]);
         });
 
         $app->run();
     }
-}
-
-__main__();
+);
