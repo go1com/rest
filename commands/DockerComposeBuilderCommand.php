fa36f3144b59906e0caad8a9d5131d2214786297
Andy Truong <thehongtt@gmail.com>
2019-03-01 10:33:51 +1000

    Add rest-cli.php

---
+++ commands/DockerComposeBuilderCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace go1\rest\commands;
+
+use go1\rest\wrapper\Manifest;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Yaml;
+
+class DockerComposeBuilderCommand extends Command
+{
+    public function __construct()
+    {
+        parent::__construct('docker-compose');
+
+        $this->addArgument('path', InputArgument::REQUIRED, 'Path to manifest file.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        /** @var Manifest $manifest */
+        $manifest = require $input->getArgument('path');
+        $_ = $manifest->dockerCompose()->build();
+        $_ = Yaml::dump($_, Yaml::DUMP_OBJECT_AS_MAP);
+
+        echo $_;
+    }
+}
---
+++ rest-cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace go1\rest;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use go1\rest\commands\ComposerBuilderCommand;
+use go1\rest\commands\DockerComposeBuilderCommand;
+use Symfony\Component\Console\Application;
+
+$cli = new Application();
+$cli->add(new DockerComposeBuilderCommand);
+$cli->add(new ComposerBuilderCommand);
+$cli->run();
