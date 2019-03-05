2091e1b44d5ac88603564e9ec86ffe5d8a436233
Andy Truong <thehongtt@gmail.com>
2019-03-05 12:08:43 +1000

    JwtParserTest > Simpler.

--- tests/http/JwtParserTest.php
+++ tests/http/JwtParserTest.php
@@ -9,11 +9,6 @@ use go1\rest\tests\RestTestCase;
 
 class JwtParserTest extends RestTestCase
 {
-    /**
-     * @var RestService
-     */
-    protected $app;
-
     protected $jwt =
         'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnbzEudXNlciIsInZlciI6In
         YxOC4xMi4wMy4wIiwiZXhwIjoxNTU0Mzk1OTAzLCJvYmplY3QiOnsidHlwZSI6InVzZXIiLC
@@ -25,25 +20,20 @@ class JwtParserTest extends RestTestCase
         cwLCJwcm9maWxlX2lkIjoxMywicm9sZXMiOlsiYWRtaW5pc3RyYXRvciIsIlN0dWRlbnQiXX
         1dfX19.q0kLPi9ynbQJQTOw5IMtDhj4h4nA2u1eowZbIkkE-uw';
 
-    protected function server()
+    protected function install(RestService $rest)
     {
-        $this->app->get('/auth', function (Request $request, Response $response) {
+        parent::install($rest);
+
+        $rest->get('/auth', function (Request $request, Response $response) {
             return $response->withJson($request->contextUser());
         });
-    }
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        $this->app = $this->rest();
-        $this->server();
     }
 
     public function testCanParseQuery()
     {
         $req = $this->mf()->createRequest('GET', '/auth?jwt=' . $this->jwt);
-        $res = $this->app->process($req, new Response());
-        $payload = json_decode($res->getBody());
+        $res = $this->rest()->process($req, new Response());
+        $payload = $res->json();
 
         $this->assertEquals(200, $res->getStatusCode());
         $this->assertEquals('phuong.huynh@go1.com', $payload->mail);
@@ -55,7 +45,7 @@ class JwtParserTest extends RestTestCase
             ->mf()
             ->createRequest('GET', '/auth')
             ->withHeader('authorization', 'Bearer ' . $this->jwt);
-        $res = $this->app->process($req, new Response());
+        $res = $this->rest()->process($req, new Response());
         $payload = json_decode($res->getBody());
 
         $this->assertEquals(200, $res->getStatusCode());
