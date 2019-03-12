f4b2a8e2ccca3cbe15abd89e7738b5fd092c7db6
Phuong Huynh <thanhphuongat@gmail.com>
2019-03-12 16:36:11 +0700

    Add mock db example test

--- examples/resources/config.default.php
+++ examples/resources/config.default.php
@@ -7,4 +7,7 @@ return [
     'noDefaultEndpoint'            => true,
     'settings.displayErrorDetails' => true,
     Logger::class                  => function () { return new Logger('example'); },
+    'dbOptions' => [
+        'default' => 'mysql://go1:go1@localhost:3306/go1_dev',
+    ]
 ];
