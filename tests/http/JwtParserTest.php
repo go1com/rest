4de44acd779b341885668315921fc06b272e4279
Andy Truong <thehongtt@gmail.com>
2019-03-18 17:02:47 +1000

    Add JwtParserTest::SYSTEM_USER.

--- tests/http/JwtParserTest.php
+++ tests/http/JwtParserTest.php
@@ -9,6 +9,9 @@ use go1\rest\tests\RestTestCase;
 
 class JwtParserTest extends RestTestCase
 {
+    // constant for other test cases to reuse.
+    CONST SYSTEM_USER = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnbzEudXNlciIsInZlciI6InYxOC4xMi4wMy4wIiwiZXhwIjoxNTU0Mzk1OTAzLCJvYmplY3QiOnsidHlwZSI6InVzZXIiLCJjb250ZW50Ijp7ImlkIjo0MTY2OCwiaW5zdGFuY2UiOiJhY2NvdW50cy1kZXYuZ29jYXRhbHl6ZS5jb20iLCJtYWlsIjoic3lzdGVtQGxvY2FsIiwibmFtZSI6IlN5c3RlbSBVc2VyIiwicm9sZXMiOlsiQWRtaW4gb24gI0FjY291bnRzIl19fX0.kEKp1dQ5tJRVeUQQ_j47s2BhRJnreMRP_XpsAlNaXcI';
+
     protected $jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJnbzEudXNlciIsInZlciI6InYxOC4xMi4wMy4wIiwiZXhwIjoxNTU0Mzk1OTAzLCJvYmplY3QiOnsidHlwZSI6InVzZXIiLCJjb250ZW50Ijp7ImlkIjo0MTY2OCwiaW5zdGFuY2UiOiJhY2NvdW50cy1kZXYuZ29jYXRhbHl6ZS5jb20iLCJwcm9maWxlX2lkIjoxNjY5NiwibWFpbCI6InBodW9uZy5odXluaEBnbzEuY29tIiwibmFtZSI6IlBodW9uZyBIdXluaCIsInJvbGVzIjpbIkFkbWluIG9uICNBY2NvdW50cyIsImRldmVsb3BlciIsInRyYWluaW5nIGFjY291bnQgbWFuYWdlciJdLCJhY2NvdW50cyI6W3siaWQiOjQ4NzU3LCJpbnN0YW5jZSI6InFhLm15Z28xLmNvbSIsInBvcnRhbF9pZCI6NTAwNDcwLCJwcm9maWxlX2lkIjoxMywicm9sZXMiOlsiYWRtaW5pc3RyYXRvciIsIlN0dWRlbnQiXX1dfX19.q0kLPi9ynbQJQTOw5IMtDhj4h4nA2u1eowZbIkkE-uw';
 
     protected function install(RestService $rest)
@@ -40,5 +43,15 @@ class JwtParserTest extends RestTestCase
 
         $this->assertEquals(200, $res->getStatusCode());
         $this->assertEquals('phuong.huynh@go1.com', $res->json()->mail);
+    }
+
+    public function testSystemUser()
+    {
+        $this->assertTrue(
+            $this
+                ->mf()
+                ->createRequest('GET', '/auth?jwt=' . static::SYSTEM_USER)
+                ->isSystemUser()
+        );
     }
 }
