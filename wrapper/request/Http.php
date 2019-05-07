eb79a15d3277c2df277f0f48893952f8ec84d945
Andy Truong <thehongtt@gmail.com>
2019-05-07 13:23:58 +1000

    go1/rest
    Add wrapper/request/Http as helper to send HTTP request.

---
+++ wrapper/request/Http.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace go1\rest\wrapper\request;
+
+use go1\rest\wrapper\ServiceUrl;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * Example:
+ *
+ *      $client = $container->get(HttpClient::class);
+ *      $client->sendRequest(
+ *          $client
+ *              ->createRequest(
+ *                  'POST',
+ *                  $client
+ *                      ->serviceUr('user', '/login')
+ *                      ->withQuery('csrf-token', 'some-thing')
+ *              )
+ *              ->withBody($client->createStream('{"username": "my-name", "password": "my-password"}'))
+ *      );
+ */
+class Http
+{
+    private $client;
+    private $sf;
+    private $serviceUrl;
+
+    public function __construct(ClientInterface $client, Psr17Factory $sf, ServiceUrl $serviceUrl)
+    {
+        $this->client = $client;
+        $this->sf = $sf;
+        $this->serviceUrl = $serviceUrl;
+    }
+
+    /**
+     * @param string              $method
+     * @param string|UriInterface $uri
+     * @return RequestInterface
+     */
+    public function createRequest(string $method, $uri): RequestInterface
+    {
+        return $this->sf->createRequest($method, $uri);
+    }
+
+    public function createStream(string $content): StreamInterface
+    {
+        return $this->sf->createStream($content);
+    }
+
+    public function sendRequest(RequestInterface $req): ResponseInterface
+    {
+        return $this->client->sendRequest($req);
+    }
+
+    public function serviceUri(string $service, string $path = ''): UriInterface
+    {
+        return $this->sf->createUri(
+            $this->serviceUrl->get($service, $path)
+        );
+    }
+}
