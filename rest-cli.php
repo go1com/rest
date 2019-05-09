537e8be2d171c5636801eb7a864f1834d3eee801
Andy Truong <thehongtt@gmail.com>
2019-05-09 10:07:12 +1000

    go1/rest > cli
    Support hook.

--- rest-cli.php
+++ rest-cli.php
@@ -4,18 +4,27 @@
 namespace go1\rest;
 
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
+    $loader = require __DIR__ . '/vendor/autoload.php';
 } else {
-    require __DIR__ . '/../../autoload.php';
+    $loader = require __DIR__ . '/../../autoload.php';
 }
 
 use go1\rest\commands\ComposerBuilderCommand;
 use go1\rest\commands\DockerComposeBuilderCommand;
 use go1\rest\commands\SwaggerBuilderCommand;
 use Symfony\Component\Console\Application;
+use function call_user_func;
+use function file_exists;
+use function getenv;
 
 $cli = new Application();
 $cli->add(new DockerComposeBuilderCommand);
 $cli->add(new ComposerBuilderCommand);
 $cli->add(new SwaggerBuilderCommand);
+
+$hook = getenv('REST_CLI_HOOK');
+if ($hook && file_exists($hook)) {
+    call_user_func(require $hook, $loader, $cli);
+}
+
 $cli->run();
