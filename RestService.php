a89ee0797e5e8d786512f9da7d71a3ce25fab82f
Andy Truong <thehongtt@gmail.com>
2019-05-31 13:37:17 +1000

    verbose error.

--- RestService.php
+++ RestService.php
@@ -33,7 +33,6 @@ class RestService extends \DI\Bridge\Slim\App
     const SYSTEM_USER = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvYmplY3QiOnsidHlwZSI6InVzZXIiLCJjb250ZW50Ijp7ImlkIjoxLCJwcm9maWxlX2lkIjoxLCJyb2xlcyI6WyJBZG1pbiBvbiAjQWNjb3VudHMiXSwibWFpbCI6IjFAMS4xIn19fQ.YwGrlnegpd_57ek0vew5ixBfzhxiepc5ODVwPva9egs';
 
     private $cnf;
-
     private $name;
 
     public function __construct(array $cnf = [])
